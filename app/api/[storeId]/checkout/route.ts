11a798dbb6d4dc24a97d161da7188344147021a6
shahriar-bhuiyan <Shahriarbhuiyan1@gmail.com>
2023-12-30 13:30:56 +0600

    "stripe"

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import { stripe } from "@/lib/stripe";
 
 import prismadb from "@/lib/prismadb";
+import { string } from "zod";
 
 
 
@@ -14,4 +15,40 @@ const corsHeaders = {
 
 export async function OPTIONS() {
     return NextResponse.json({},{headers:corsHeaders})
+}
+
+export async function POST (req:Request,
+    {params}:{params:{storeId:string}}){
+    
+
+    const {productIds} = await req.json();
+
+    if(!productIds || productIds.length === 0){
+       return new NextResponse("Product ids are required",{status:400})
+    }
+
+    const products = await prismadb.product.findMany({
+        where:{
+            id:{
+                in:productIds
+            }
+        }
+    });
+
+    const line_items:Stripe.Checkout.SessionCreateParams.LineItem[]= [];
+
+    products.forEach((product)=>{
+        line_items.push({
+            quantity:1,
+            price_data:{
+                currency:'BDT',
+                product_data:{
+                    name:product.name
+                },
+                unit_amount:product.price.toNumber()*100
+            }
+        })
+    })
+
+
 }
