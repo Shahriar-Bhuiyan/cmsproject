1488c8b609e397e2d82a050495f5c735c6317be8
shahriar-bhuiyan <Shahriarbhuiyan1@gmail.com>
2023-12-30 12:49:47 +0600

    "stripe"

---
+++ app/api/[storeId]/checkout/route.ts
@@ -0,0 +1,17 @@
+import Stripe from "stripe";
+import { NextResponse } from "next/server";
+import { stripe } from "@/lib/stripe";
+
+import prismadb from "@/lib/prismadb";
+
+
+
+const corsHeaders = {
+    "Access-Control-Allow-Origin":"*",
+    "Access-Control-Allow-Methods":"GET,POST,PUT,DELETE,OPTIONS",
+    "Access-Control-Allow-Headers":"Content-Type,Authorizatioin",
+}
+
+export async function OPTIONS() {
+    return NextResponse.json({},{headers:corsHeaders})
+}
