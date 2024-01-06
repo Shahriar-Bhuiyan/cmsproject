5d75d26802ee709137602df698a7e6be80371438
shahriar-bhuiyan <Shahriarbhuiyan1@gmail.com>
2024-01-06 13:58:02 +0600

    products-cell-action

--- app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
@@ -30,32 +30,29 @@ export const CellActions:React.FC<CellActionProps> = ({data})=>{
     const [loading,setLoading] = useState<boolean>(false)
     const [open,setOpen] = useState<boolean>(false)
   
-    const onCopy = (id:string)=>{
-        navigator.clipboard.writeText(id)
-        toast.success('Billboard Id copied to the clipboard')
+    const onConfirm = async () => {
+      try {
+        setLoading(true);
+        await axios.delete(`/api/${params.storeId}/products/${data.id}`);
+        toast.success('Product deleted.');
+        router.refresh();
+      } catch (error) {
+        toast.error('Something went wrong');
+      } finally {
+        setLoading(false);
+        setOpen(false);
+      }
+    };
+  
+    const onCopy = (id: string) => {
+      navigator.clipboard.writeText(id);
+      toast.success('Product ID copied to clipboard.');
     }
-    
-
-    const onDelete = async () => {
-        try {
-          setLoading(true);
-    
-          await axios.delete(`/api/${params.storeId}/products/${data.id}`);
-          router.refresh();
-          router.push(`/${params.storeId}/products`);
-          toast.success("Products Deleted successfully");
-        } catch (error) {
-          toast.error("Make sure you remove all categories using this Products");
-        } finally {
-          setLoading(false);
-          setOpen(false);
-        }
-      };
     return (
     <>
     <AlertModal isOpen={open} 
     onClose={()=>setOpen(false)} 
-    onConfirm={onDelete} 
+    onConfirm={onConfirm} 
     loading={loading}/>
 
     <DropdownMenu>
